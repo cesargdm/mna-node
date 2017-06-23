f7f22a548bbfc21222f11b2e49ce31a58cf1555b
Liliana Romualdo Osorio <osorli@mx1.ibm.com>
2017-06-23 11:10:13 -0500

    new workspaces id

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -5,43 +5,43 @@ const initialPieces = [
     name: 'piedra-del-sol',
     title: 'Piedra del Sol',
     location: 'Sala Mexica',
-    workspace_id: ''
+    workspace_id: '3a64558c-6b92-429a-88c6-97ddb5717a34'
   },
   {
     name: 'coatlicue',
     title: 'Coatlicue',
     location: 'Sala Mexica',
-    workspace_id: ''
+    workspace_id: '079540b1-6c6c-499e-b9bd-9650d5e6c33c'
   },
   {
     name: 'penacho-de-moctezuma',
     title: 'Penacho de Moctezuma',
     location: 'Sala Mexica',
-    workspace_id: ''
+    workspace_id: '14b361e3-5852-49f7-96a0-5b40c4ecd476'
   },
   {
     name: 'dintel-26',
     title: 'Dintel 26',
     location: 'Sala Maya',
-    workspace_id: '91520396-535c-409c-b1a7-60e2724ec8ba'
+    workspace_id: 'f76b61b0-9158-45d8-a936-a8cfbcda3be9'
   },
   {
     name: 'tumba-de-pakal',
     title: 'Tumba de Pakal',
     location: 'Sala Maya',
-    workspace_id: '976dbd0a-20eb-4898-94e0-3c3a6dc6526f'
+    workspace_id: '4b3d7feb-5f85-4ee2-94e8-0a4193a6163f'
   },
   {
     name: 'chac-mool',
     title: 'Chac-Mool',
     location: 'Sala Maya',
-    workspace_id: '4f9a0e7d-41c1-4505-846e-3998418a4e10'
+    workspace_id: '5a4dc283-6483-4a8d-b1cb-c9df50e3d49d'
   },
   {
     name: 'mural-dualidad',
     title: 'Mural Dualidad',
     location: 'General',
-    workspace_id: '2bf70448-d22c-4f7f-b220-d3f2fe25249b'
+    workspace_id: '6670d9ff-1ec4-4751-9971-27e45f6912b4'
   }
 ]
 
