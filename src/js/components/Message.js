2f8c44fbb6b4e44cedfa5cf3f103d6e4837766e6
César Guadarrama <cesarguadarrama@icloud.com>
2017-07-06 11:45:29 -0500

    Revoved answer rater

--- src/js/components/Message.js
+++ src/js/components/Message.js
@@ -5,23 +5,6 @@ function Message(props) {
     <div className={props.element.answer ? 'answer' : 'question'} >
       { props.element.text }
       { props.unReviewable}
-      {
-        (props.element.answer && !props.unReviewable) ?
-        <div className='rater'>
-          <p>Califica esta respuesta</p>
-
-          {
-            props.reviewed
-            ? <span><strong>Gracias por tu ayuda</strong></span>
-            : <div className="rate-container">
-                <div className='good' onClick={() => props.onRate(props.element.text, true, props.index)}></div>
-                <div className='bad' onClick={() => props.onRate(props.element.text, false, props.index)}></div>
-              </div>
-          }
-
-        </div>
-        : null
-      }
     </div>
   )
 }
